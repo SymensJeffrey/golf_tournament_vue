fced36fe1198b2a088a9ef1e539fa1ca2c0b23d6
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-09-15 14:05:25 -0500

    add course name and hole column

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,97 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          {{golfCourseName}}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          Hole
+        </div>
+        <div class="col">
+          1
+        </div>
+        <div class="col">
+          2
+        </div>
+        <div class="col">
+          3
+        </div>
+        <div class="col">
+          4
+        </div>
+        <div class="col">
+          5
+        </div>
+        <div class="col">
+          6
+        </div>
+        <div class="col">
+          7
+        </div>
+        <div class="col">
+          8
+        </div>
+        <div class="col">
+          9
+        </div>
+        <div class="col">
+          Out
+        </div>
+        <div class="col">
+          10
+        </div>
+        <div class="col">
+          11
+        </div>
+        <div class="col">
+          12
+        </div>
+        <div class="col">
+          13
+        </div>
+        <div class="col">
+          14
+        </div>
+        <div class="col">
+          15
+        </div>
+        <div class="col">
+          16
+        </div>
+        <div class="col">
+          17
+        </div>
+        <div class="col">
+          18
+        </div>
+         <div class="col">
+          In
+        </div>
+         <div class="col">
+          Total
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.col{
+  border-style: solid;
+  border-width: 1px;
+}
+</style>
 
 <script>
   export default {
     data: function () {
       return {
         message: "Welcome to the 2022 Symens Open",
+        golfCourseName: "Pebble Beach"
       };
     },
     created: function () {},
